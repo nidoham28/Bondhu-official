0e862670b133535a002a2ed8fa632afce2531ae8
nidoham28 <nidohamondol@gmail.com>
2026-03-09 13:10:51 +0600

    Fixed added workflow features

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.nidoham.bondhu"
         minSdk = 23
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.0.1-alpha"
+        versionCode = 3
+        versionName = "1.0.2-alpha"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
